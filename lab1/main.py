2fdf6e55a348cc11733deedfe27d102380663707
Nikolay <94075184+KLARKOFF@users.noreply.github.com>
2025-01-11 22:42:38 +0300

    Добавил первую лабораторную

---
+++ lab1/main.py
@@ -0,0 +1,17 @@
+# 1
+
+number = int(input("Введите число"))
+if number > 0:
+  for i in range(1, number + 1):
+    print(i)
+
+# 2
+
+num1 = int(number("Введите число"))
+num2 = int(number("Введите число"))
+if num1 > num2:
+  print(num1)
+else:
+  print(num2)
+
+#  print(f"Большее число: {max(num1, num2)}")
